b4666ea4720e1cf810a35b6621c9d85b32ddd7a3
Jacquese <jacqwhitson@gmail.com>
2023-03-12 15:40:28 -0400

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Jacquese Whitson  on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
